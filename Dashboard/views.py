22fd89aeea4f75d19d7de340475a65589e471509
darknorth10 <abbyalbus123@gmail.com>
2022-04-27 18:34:37 +0800

    none

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -60,8 +60,9 @@ def profile(request):
 
 
 # update Profile
-def update_profile(request):
-  user_form = UserUpdateForm(request.POST, instance=request.user)
+def update_profile(request, id):
+  obj = get_object_or_404(User, id = id)
+  user_form = UserUpdateForm(request.POST, instance=obj)
   profile_form = updateProfile(request.POST, request.FILES, instance=request.user.profile)
   if request.method == 'POST':
     
