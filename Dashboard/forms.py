f416ab58190adcb87d6e969e5f3d52cc4c5988b1
darknorth10 <abbyalbus123@gmail.com>
2022-04-28 11:59:29 +0800

    none

--- Dashboard/forms.py
+++ Dashboard/forms.py
@@ -25,16 +25,15 @@ class updateTaskForm(UpdateView):
       
 # update profile
 class UserUpdateForm(forms.ModelForm):
-  email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-  first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-  last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+  first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+  last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
   class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name']
+        fields = ['first_name', 'last_name']
         
 class updateProfile(forms.ModelForm):
   image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-  quote = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+  quote = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
   
   class Meta:
         model = Profile
